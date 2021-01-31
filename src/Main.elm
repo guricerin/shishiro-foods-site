a8c24e44a255cd245903b889488a86c88a48248e
guricerin <chanbo1e9@gmail.com>
2021-01-31 16:14:11 +0900

    elm-spa

--- src/Main.elm
+++ src/Main.elm
@@ -2,34 +2,53 @@ module Main exposing (main)
 
 import Browser
 import Browser.Navigation as Nav
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Route exposing (Route(..))
-import Url
+import Shared exposing (Flags)
+import Spa.Document as Document exposing (Document)
+import Spa.Generated.Pages as Pages
+import Spa.Generated.Route as Route exposing (Route)
+import Url exposing (Url)
+
+
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view >> Document.toBrowserDocument
+        , onUrlRequest = LinkClicked
+        , onUrlChange = UrlChanged
+        }
 
 
 
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
-
-
-
--- MODEL
+-- INIT
 
 
 type alias Model =
-    { key : Nav.Key
-    , url : Url.Url
+    { shared : Shared.Model
+    , page : Pages.Model
     }
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init _ url key =
-    ( Model key url, Cmd.none )
+init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    let
+        ( shared, sharedCmd ) =
+            Shared.init flags url key
+
+        ( page, pageCmd ) =
+            Pages.init (fromUrl url) shared
+
+        savedShare =
+            Pages.save page shared
+    in
+    ( Model savedShare page
+    , Cmd.batch
+        [ Cmd.map Shared sharedCmd
+        , Cmd.map Pages pageCmd
+        ]
+    )
 
 
 
@@ -38,196 +57,92 @@ init _ url key =
 
 type Msg
     = LinkClicked Browser.UrlRequest
-    | UrlChanged Url.Url
+    | UrlChanged Url
+    | Shared Shared.Msg
+    | Pages Pages.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        LinkClicked urlRequest ->
-            case urlRequest of
-                -- 同一ドメイン
-                Browser.Internal url ->
-                    ( model, Nav.pushUrl model.key (Url.toString url) )
+        LinkClicked (Browser.Internal url) ->
+            ( model
+            , Nav.pushUrl model.shared.key (Url.toString url)
+            )
 
-                -- 外部ドメイン
-                Browser.External href ->
-                    ( model, Nav.load href )
+        LinkClicked (Browser.External href) ->
+            ( model
+            , Nav.load href
+            )
 
         UrlChanged url ->
-            ( { model | url = url }
-            , Cmd.none
+            let
+                original =
+                    model.shared
+
+                shared =
+                    { original | url = url }
+
+                ( page, pageCmd ) =
+                    Pages.init (fromUrl url) shared
+            in
+            ( { model | page = page, shared = Pages.save page shared }
+            , Cmd.map Pages pageCmd
+            )
+
+        Shared sharedMsg ->
+            let
+                ( shared, sharedCmd ) =
+                    Shared.update sharedMsg model.shared
+
+                ( page, pageCmd ) =
+                    Pages.load model.page shared
+            in
+            ( { model | page = page, shared = shared }
+            , Cmd.batch
+                [ Cmd.map Shared sharedCmd
+                , Cmd.map Pages pageCmd
+                ]
+            )
+
+        Pages pageMsg ->
+            let
+                ( page, pageCmd ) =
+                    Pages.update pageMsg model.page
+
+                shared =
+                    Pages.save page model.shared
+            in
+            ( { model | page = page, shared = shared }
+            , Cmd.map Pages pageCmd
             )
 
 
-
--- VIEW
-
-
-view : Model -> Browser.Document Msg
+view : Model -> Document Msg
 view model =
-    { title = "ししろフーズ"
-    , body =
-        [ div []
-            [ viewHeader
-            , img [ src "%PUBLIC_URL%/assets/images/logo.svg" ] []
-            , div [ id "container" ] [ viewPage model ]
-            , viewFooter
-            ]
-        ]
-    }
-
-
-viewHeader : Html Msg
-viewHeader =
-    header [ id "header" ]
-        [ div [ class "menu_wrapper" ]
-            [ div [ class "menu" ]
-                [ ul []
-                    [ li [] [ a [ Route.href Route.Home ] [ text "Home" ] ]
-                    , li [] [ a [ Route.href Route.News ] [ text "News" ] ]
-                    , li [] [ a [ Route.href Route.Works ] [ text "Works" ] ]
-                    , li [] [ a [ Route.href Route.Overview ] [ text "Overview" ] ]
-                    , li [] [ a [ Route.href Route.Caution ] [ text "Caution" ] ]
-                    ]
-                ]
-            ]
-        ]
-
-
-viewFooter : Html Msg
-viewFooter =
-    footer [ id "sitemap" ] [ h2 [] [ text "サイトマップ" ] ]
-
-
-viewPage : Model -> Html Msg
-viewPage model =
-    case Route.fromUrl model.url of
-        Nothing ->
-            viewNotFound
-
-        Just Home ->
-            viewHome
-
-        Just News ->
-            viewNews
-
-        Just Overview ->
-            viewOverview
-
-        Just Works ->
-            viewWorks
-
-        Just Caution ->
-            viewCaution
-
-
-viewNotFound : Html Msg
-viewNotFound =
-    div []
-        [ div [] [ h2 [] [ text "404" ] ]
-        ]
-
-
-viewHome : Html Msg
-viewHome =
-    div []
-        [ div [] [ h2 [] [ text "ららーいおん" ] ]
-        ]
-
-
-viewNews : Html Msg
-viewNews =
-    div []
-        [ div [] [ h2 [] [ text "ニュース一覧" ] ]
-        ]
-
-
-viewOverview : Html Msg
-viewOverview =
-    div []
-        [ div [] [ h2 [] [ text "会社概要" ] ]
-        , section []
-            [ table []
-                [ tbody []
-                    [ tableItem "社名" "ししろフーズ"
-                    , tableItem "資本金" "0円"
-                    , tableItem "所在地" "ホロ鯖 5期生ハウス4F"
-                    , tr []
-                        [ th [] [ text "事業内容" ]
-                        , td [] [ ol [] [ li [] [ text "ラーメン店の経営" ], li [] [ text "ホロ鯖内施設の設計・施工" ] ] ]
-                        ]
-                    , tableItem "代表取締役" "獅白ぼたん"
-                    , tableItem "企業理念" "「自由」"
-                    ]
-                ]
-            ]
-        ]
-
-
-tableItem : String -> String -> Html Msg
-tableItem thstr tdstr =
-    tr [] [ th [] [ text thstr ], td [] [ text tdstr ] ]
-
-
-viewWorks : Html Msg
-viewWorks =
-    div []
-        [ div []
-            [ h2 [] [ text "実績" ]
-            , section []
-                [ ul []
-                    [ viewWork "" "2020/11/07" "麺屋ぼたん2号店"
-                    , viewWork "" "2020/10/29" "麺屋ぼたん本店"
-                    ]
-                ]
-            ]
-        ]
-
-
-viewWork : String -> String -> String -> Html Msg
-viewWork imgPath date workName =
-    li []
-        [ a [ class "" ]
-            [ img [ src imgPath ] []
-            , h3 []
-                [ text date
-                , br [] []
-                , text workName
-                ]
-            ]
-        ]
-
-
-viewCaution : Html Msg
-viewCaution =
-    div []
-        [ div [] [ h2 [] [ text "注意事項" ] ]
-        , ul []
-            [ li [] [ text "当サイトは非公式・非公認のネタサイトです。" ]
-            , li []
-                [ text "問題等ありましたら、"
-                , a [ href "https://twitter.com/guricerin" ] [ text "作者のTwitter" ]
-                , text "にお知らせ頂くか"
-                , a [ href "https://github.com/guricerin/test" ] [ text "GitHubリポジトリ" ]
-                , text "にてIssueの発行をお願い致します。"
-                ]
-            , li [] [ text "当サイトは予告なく削除する場合があります。" ]
-            ]
-        ]
-
-
-
--- MAIN
-
-
-main : Program () Model Msg
-main =
-    Browser.application
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        , onUrlChange = UrlChanged
-        , onUrlRequest = LinkClicked
+    Shared.view
+        { page =
+            Pages.view model.page
+                |> Document.map Pages
+        , toMsg = Shared
         }
+        model.shared
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Shared.subscriptions model.shared
+            |> Sub.map Shared
+        , Pages.subscriptions model.page
+            |> Sub.map Pages
+        ]
+
+
+
+-- URL
+
+
+fromUrl : Url -> Route
+fromUrl =
+    Route.fromUrl >> Maybe.withDefault Route.NotFound
