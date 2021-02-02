f8b527cc2f5aa8f5771e268a7b02df3792cdb5c4
guricerin <chanbo1e9@gmail.com>
2021-02-02 20:15:22 +0900

    index page show a random word

--- src/Pages/Top.elm
+++ src/Pages/Top.elm
@@ -1,11 +1,27 @@
 module Pages.Top exposing (Model, Msg, Params, page)
 
+import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Random
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
 import Spa.Url exposing (Url)
 import Util.Title as T
+
+
+page : Page Params Model Msg
+page =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- INIT
 
 
 type alias Params =
@@ -13,27 +29,72 @@ type alias Params =
 
 
 type alias Model =
-    Url Params
+    { selected : Maybe String }
 
 
-type alias Msg =
-    Never
+words : Array String
+words =
+    [ "ららーいおん"
+    , "ぱおん"
+    , "がう"
+    , "がうがう"
+    , "ららーめん"
+    , "オレサマ\u{3000}オマエ\u{3000}マルカジリ"
+    , "じんぎすかーん"
+    , "おはようから おやすみまで 暮らしを 見つめているぞ"
+    ]
+        |> Array.fromList
 
 
-page : Page Params Model Msg
-page =
-    Page.static
-        { view = view
-        }
+selectRandom : Cmd Msg
+selectRandom =
+    Random.int 0 (Array.length words - 1) |> Random.generate RandomNumber
+
+
+init : Url Params -> ( Model, Cmd Msg )
+init { params } =
+    ( { selected = Nothing }, selectRandom )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = RandomNumber Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        RandomNumber n ->
+            ( { model | selected = words |> Array.get n }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
 -- VIEW
 
 
-view : Url Params -> Document Msg
-view { params } =
+view : Model -> Document Msg
+view model =
     { title = T.title
     , body =
-        [ div [] [ h2 [] [ text "ららーいおん" ] ] ]
+        [ div [] [ h2 [] [ text "Shishiro Foods Corp." ] ]
+        , div [] [ h3 [] [ renderText model.selected ] ]
+        ]
     }
+
+
+renderText : Maybe String -> Html msg
+renderText selected =
+    case selected of
+        Just s ->
+            text s
+
+        Nothing ->
+            text "<nothing>"
